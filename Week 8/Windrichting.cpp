ef8d11c4e8be9d8c147f070464563d715ec1c768
DavitadJ <74309804+DavitadJ@users.noreply.github.com>
2021-06-18 14:27:24 +0200

    Add files via upload
    windrichting via gyroscoop

---
+++ Week 8/Windrichting.cpp
@@ -0,0 +1,40 @@
+//#include "mrBeen.h"
+
+void writeWind();
+
+int16_t turn = 0;
+bool turning = 0;
+uint8_t gyro_z;
+
+
+
+void writeWind(){								//leest gyro_z uit en verandert de waarde van turning op basis van die data. Output is writeString(windrichting)
+	if((gyro_z > 190) & turning){
+		turn++;
+	}
+	if((gyro_z > 50) & (gyro_z < 70) & turning){
+		turn--;
+	}
+	
+	if(turn > 76){
+		turn = turn%76;
+	}
+	if(turn < 0){
+		turn = turn + 76;
+	}
+	
+	if ((turn > 66) || (turn <= 9)){
+		writeString("noord");
+	}
+	if ((turn > 9) & (turn <= 28)){
+		writeString("oost");
+	}
+	if ((turn > 28) & (turn <= 47)){
+		writeString("zuid");
+	}
+	if ((turn > 47) & (turn <= 66)){
+		writeString("west");
+	}
+	//writeInt(turn);
+	writeString("\n\r");
+}
