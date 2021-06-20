737a1cabc4776984d5ece5e2c34884956d9488f0
Shaquille Soekhlal <s.soekhlal@aperturect.com>
2021-06-20 16:21:54 +0200

    change gyro function names  to i2c

--- Week 8/main.cpp
+++ Week 8/main.cpp
@@ -60,22 +60,22 @@ int main()
 {
 	twiInit();
 	
-	gyroWrite(0x20, 0b1111, SDS1307_W);
-	gyroWrite(SDS1307_CTRL_5, (1<<7),SDS1307_W);
-	gyroWrite(0x20, 0b1111, compass_w);
-	gyroWrite(compass_ctrl_0, (1<<7), compass_w);
-	// gyroWrite(compass_ctrl_M, (1<<1), compass_w);
-	// gyroWrite(compass_ctrl_M, (1<<5), compass_w);
-	// gyroWrite(compass_ctrl_M, (1<<6), compass_w);
-	// gyroWrite(compass_ctrl_M, (1<<7), compass_w);
-	// gyroWrite(compass_ctrl_M, (1<<8), compass_w);
-	// gyroWrite(compass_fifo_ctrl, (1<<6), compass_w);
-	// gyroWrite(compass_fifo_ctrl, (0<<7), compass_w);
-	// gyroWrite(compass_fifo_ctrl, (0<<8), compass_w);
-	//gyroWrite(compass_ctrl_4, (1<<5), compass_w);
-	gyroWrite(compass_ctrl_5, 0b01100100, compass_w);
-	gyroWrite(compass_ctrl_6, 0b00100000, compass_w);
-	gyroWrite(compass_ctrl_7, 0b00000000, compass_w);
+	i2cWrite(0x20, 0b1111, SDS1307_W);
+	i2cWrite(SDS1307_CTRL_5, (1<<7),SDS1307_W);
+	i2cWrite(0x20, 0b1111, compass_w);
+	i2cWrite(compass_ctrl_0, (1<<7), compass_w);
+	// i2cWrite(compass_ctrl_M, (1<<1), compass_w);
+	// i2cWrite(compass_ctrl_M, (1<<5), compass_w);
+	// i2cWrite(compass_ctrl_M, (1<<6), compass_w);
+	// i2cWrite(compass_ctrl_M, (1<<7), compass_w);
+	// i2cWrite(compass_ctrl_M, (1<<8), compass_w);
+	// i2cWrite(compass_fifo_ctrl, (1<<6), compass_w);
+	// i2cWrite(compass_fifo_ctrl, (0<<7), compass_w);
+	// i2cWrite(compass_fifo_ctrl, (0<<8), compass_w);
+	//i2cWrite(compass_ctrl_4, (1<<5), compass_w);
+	i2cWrite(compass_ctrl_5, 0b01100100, compass_w);
+	i2cWrite(compass_ctrl_6, 0b00100000, compass_w);
+	i2cWrite(compass_ctrl_7, 0b00000000, compass_w);
 	
 	//char udata;
 	USART_Init();
@@ -181,15 +181,15 @@ int main()
 void Gyro(){
 			writeString("Gyro = { ");
 				
-				gyroRead(SDS1307_GYRO_X_H, SDS1307_W, SDS1307_R);
+				i2cRead(SDS1307_GYRO_X_H, SDS1307_W, SDS1307_R);
 				writeInt(data_Read);
 				writeString("\t");
 
-				gyroRead(SDS1307_GYRO_Y_H, SDS1307_W, SDS1307_R);
+				i2cRead(SDS1307_GYRO_Y_H, SDS1307_W, SDS1307_R);
 				writeInt(data_Read);
 				writeString("\t");
 
-				gyroRead(SDS1307_GYRO_Y_H, SDS1307_W, SDS1307_R);
+				i2cRead(SDS1307_GYRO_Y_H, SDS1307_W, SDS1307_R);
 				writeInt(data_Read);
 				if(data_Read > 100){
 					rightturn++;
@@ -204,16 +204,16 @@ void Gyro(){
 
 void compass(){
 			writeString("compass = { ");
-				//gyroRead(0x0F, compass_w, compass_r);
-				gyroRead(compass_out_x_H, compass_w, compass_r);
+				//i2cRead(0x0F, compass_w, compass_r);
+				i2cRead(compass_out_x_H, compass_w, compass_r);
 				 writeInt(data_Read);
 				 writeString("\t");
 
-				gyroRead(compass_out_y_H, compass_w, compass_r);
+				i2cRead(compass_out_y_H, compass_w, compass_r);
 				writeInt(data_Read);
 				writeString("\t");
 
-				gyroRead(compass_out_z_H, compass_w, compass_r);
+				i2cRead(compass_out_z_H, compass_w, compass_r);
 				writeInt(data_Read);
 				writeString(" ");
 				
